13000f6d9514a00f1f93403837494b57ec4cc65e
Mark111P <markusperevezentsev@gmail.com>
2024-04-03 17:43:55 +0200

    Добавьте файлы проекта.

---
+++ ASP(3)/Pages/countries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_3_.Pages
+{
+    public class countriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
